b5073aa365d4644fc892e027cc504b3584e52b51
alienfoxx <alienfoxx001@gmail.com>
2024-04-19 20:56:48 +0100

    finish the hider slider

---
+++ amzone-clone/main.js
@@ -0,0 +1,30 @@
+const images = document.querySelectorAll(".header-slider ul img");
+const nextBtn = document.querySelector(".control-next");
+const prevBtn = document.querySelector(".control-prev");
+
+let currentImg = 0;
+
+prevBtn.addEventListener("click", (e) => {
+  if (currentImg > 0) {
+    currentImg--;
+  } else {
+    currentImg = images.length - 1;
+  }
+  changeSlide();
+});
+nextBtn.addEventListener("click", (e) => {
+  if (currentImg == images.length - 1) {
+    currentImg = 0;
+  } else {
+    currentImg++;
+  }
+  changeSlide();
+});
+
+function changeSlide() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  images[currentImg].style.display = "block";
+}
+changeSlide();
