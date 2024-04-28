361cf4f54d9e7c1a22b35fa581fdb07d96d9fe47
alienfoxx <alienfoxx001@gmail.com>
2024-04-28 18:10:28 +0100

    finish the slider and anouncement componenet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
+import Anouncement from "../componenet/Anouncement"
 import Navbar from "../componenet/Navbar"
+import Slider from "../componenet/Slider"
 
 
 
 const Home = () => {
   return (
     <div>
+        <Anouncement/>
         <Navbar/>
+        <Slider/>
     </div>
   )
 }
